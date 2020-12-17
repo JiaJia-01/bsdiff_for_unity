6b4ce60115483ed7a827533a18e18bde3f1f78a8
罗佳 <208608@nd.com>
2020-12-17 16:57:46 +0800

    unity proj

---
+++ bsdiff_unity/Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    [DllImport("bsdiff")]        
+    private static extern int test(int a, int b);
+
+    void Start()
+    {
+        GetComponent<Text>().text = test(1, 2).ToString();
+    }
+}
